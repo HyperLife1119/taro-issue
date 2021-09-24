b3e4515fdefdce0bfd1c0f82ef71639a7b01f5d1
HyperLife1119 <hyperlife1119@qq.com>
2021-09-24 15:26:10 +0800

    init

---
+++ src/pages/index/index.config.ts
@@ -0,0 +1,6 @@
+export default {
+  navigationBarTitleText: '首页', usingComponents: {
+    'van-button': '/components/vant/button',
+    'van-field': '/components/vant/field',
+  }
+}
---
+++ src/pages/index/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <view class="index">
+    <van-field :readonly="true" :value="msg" label="姓名" maxlength="30" />
+    <van-button class="my-cls" type="primary">按钮</van-button>
+  </view>
+</template>
+
+<script>
+import { ref } from 'vue'
+import './index.scss'
+
+export default {
+  setup() {
+    const msg = ref('Hello world')
+    return {
+      msg
+    }
+  }
+}
+</script>
+
+<style>
+.my-cls button {
+  font-size: 50px;
+}
+</style>
